38fe3480e974d6730ae5de654bf0c734049786db
Alex Anschuetz <alexanschuetz@gmail.com>
2016-01-08 12:17:09 -0800

    improves signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,9 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      session[:user_id] = @user.id
   		redirect_to user_workouts_path(@user)
+
   	else
   		render :new
   	end
