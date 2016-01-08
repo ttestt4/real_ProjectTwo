d1e2bf9eb6e7a4f79d42550be384f0ee4e63bd55
Alex Anschuetz <alexanschuetz@gmail.com>
2016-01-08 11:40:37 -0800

    allows people to sign up lol

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:email])
   	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
-  		redirect_to user_workouts_path(user), notice: "congrats!"
+  		redirect_to user_workouts_path(user), notice: "Congrats, Logged In!"
   	else
   		render :new
   	end
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
   	session[:user_id] = nil
-  	redirect_to root_url, notice: "logged out!"
+  	redirect_to root_url, notice: "Logged Out, Peace!"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate
+  before_action :authenticate, only: [:index]
   
   def index
   	@users = User.all
