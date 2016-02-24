e54b686dd10f4536109f4c7c63fc3f394165a21e
Alex Anschuetz <alexanschuetz@gmail.com>
2016-02-23 23:38:14 -0800

    fixes searchbyexercise page along with a lot of other stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:email])
   	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
-  		redirect_to user_workouts_path(user), notice: "Congrats, Logged In!"
+  		redirect_to user_workouts_path(user)
   	else
   		render :new
   	end
